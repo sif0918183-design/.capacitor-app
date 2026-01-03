e00774059c1ffd05c0159200aa2ed29fdedf7e96
sif0918183-design <sif0918183@gmail.com>
2026-01-03 08:03:25 +0200

    Update MainActivity.java

--- android/app/src/java/com/zoona/driver/MainActivity.java
+++ android/app/src/java/com/zoona/driver/MainActivity.java
@@ -1,1 +1,18 @@
- 
+package com.zoona.driver;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://driver.zoonasd.com/");
+        setContentView(webView);
+    }
+}
